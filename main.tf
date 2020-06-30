87f5430d0be4c48eeada62ce32b3e18b82dc02ca
John Fahl <john@thedarkwriter.io>
2020-06-30 10:00:12 -0400

    Updating module endpoint to private module

--- main.tf
+++ main.tf
@@ -3,9 +3,10 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
-  name        = var.name
+  source  = "app.terraform.io/thedarkwriter/s3-webappjf/aws"
+  version = "1.0.0"
+
+  name   = var.name
   region = var.region
   prefix = var.prefix
-  version = "1.0.0"
 }
